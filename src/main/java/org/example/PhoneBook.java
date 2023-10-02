d33dc7827490df30c7d6162d2ebb0720baf81da0
MaximKorchagin <Airfallsc2@gmail.com>
2023-10-02 15:31:23 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    Map<String, String> map = new HashMap<>();
+    public int add(String name, String number) {
+        if (!map.containsKey(name)) {
+            map.put(name, number);
+        }
+        return map.size();
+    }
 
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
 
+    @Test
+    public void shouldAddToMapWhenAddNewNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Temych";
+        String number = "88005553599";
+        phoneBook.add(name, number);
+        Assertions.assertTrue(phoneBook.map.containsKey(name));
+        Assertions.assertEquals(number, phoneBook.map.get(name));
+    }
+
+    @Test
+    public void shouldReturnNumberOfContacts() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Temych";
+        String number = "88005553599";
+        int counter = phoneBook.add(name, number);
+        Assertions.assertEquals(1, counter);
+        counter = phoneBook.add("Tigran", "3523523612");
+        Assertions.assertEquals(2, counter);
+    }
+
+    @Test
+    public void shouldNotAddToMapIfNameExists() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Temych";
+        String number = "88005553599";
+        phoneBook.add(name, number);
+        int counter = phoneBook.add(name, "3243423");
+        Assertions.assertEquals(1, counter);
+        Assertions.assertEquals(number, phoneBook.map.get(name));
+    }
 }
