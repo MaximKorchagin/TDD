92186603cdde99989c361e82e88631ef305c2fe5
MaximKorchagin <Airfallsc2@gmail.com>
2023-10-02 15:42:40 +0300

    findByNumber implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -6,18 +6,17 @@ import java.util.Map;
 public class PhoneBook {
 
     Map<String, String> nameToNumber = new HashMap<>();
-
+    Map<String, String> numberToName = new HashMap<>();
     public int add(String name, String number) {
         if (!nameToNumber.containsKey(name)) {
             nameToNumber.put(name, number);
-
+            numberToName.put(number, name);
         }
         return nameToNumber.size();
     }
 
     public String findByNumber(String number) {
-
-        return null;
+        return numberToName.get(number);
     }
 
 }
